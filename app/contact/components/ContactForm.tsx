f44ff4656f687114376717c381146186d113c582
Sravani-Ravulla <Ravula.Sravani@elevancesystems.com>
2025-10-27 17:30:17 +0530

    Fixed the build Error

--- app/contact/components/ContactForm.tsx
+++ app/contact/components/ContactForm.tsx
@@ -69,7 +69,7 @@ const ContactUs = () => {
       {/* Success Message */}
       {submitStatus === 'success' && (
         <div className="mb-6 p-4 bg-green-100 border border-green-400 text-green-700 rounded w-[80%]">
-          Thank you for your message! We'll get back to you soon.
+          Thank you for your message! We&apos;ll get back to you soon.
         </div>
       )}
       
