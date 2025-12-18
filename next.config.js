3152cff751a851857ca56caa72648071c01e09ee
annaO2AI <annasaheb.kute@o2aicorp.com>
2025-12-18 12:49:44 +0530

    UI fixed rout

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'o2aicorp.com',
+          },
+        ],
+        destination: 'https://www.o2aicorp.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig
